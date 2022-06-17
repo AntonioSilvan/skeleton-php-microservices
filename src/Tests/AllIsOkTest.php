c2e89b374e282109db0f564063ff676301e9a353
AntonioSilvan <jjoosseessiillvvaann@gmail.com>
2022-06-17 00:01:03 -0500

    Agregando el primer test

---
+++ src/Tests/AllIsOkTest.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+
+namespace Src\Tests;
+
+final class AllIsOkTest extends \PHPUnit\Framework\TestCase{
+    public function testAllIsOk() {
+        $this->assertTrue(true, 'Somenthing went wrong');
+    }
+}
